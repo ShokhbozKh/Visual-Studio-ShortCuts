e3d9b824e0499a919272807beb4d60e3d088b369
Shohboz Xurramov <shohboztuit@gmail.com>
2023-11-18 00:48:22 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ctrl+g --> kerakli qatorga utish
+            //days to day--> f2 va yangi nom ctrl+r reneme yoki f2
+            // kodlarni method qilish -> ctril+m + .
+            // ctrl+ <^pastga> -- suz buylab yuradi --> kursor
+            // alt+ tepa pastga--> kodni line buylab kuchirish
+            // shift + delete --> bir qator kod tuliq uchib ketadi
+            //ctrl+tab --> tepada ochilgan cs file buylab yurish
+            //kup satrli izoh-- ctrl+shift+/ /*---*/
+            // bir xil kodni  kup qatorga yozish--> alt bilan pastga tortib belgilash kerak
+            //ctrl+M+O--> hamma kod yozilgan blocklar yopiladi -- ochish - ctrl+m+p
+
+
+
+
+        }
+
+        private static void NewMethod1()
+        {
+            string hello = "salom dunyo";
+        }
+        
+        private static void NewMethod()
+        {
+            int day = 12;
+            day = day + 1;
+            day = day + 2;
+            Console.WriteLine(day);
+        }
+    }
+}
